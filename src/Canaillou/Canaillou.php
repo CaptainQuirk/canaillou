ccf97162dc73418f4dcacca6f687300f8a4f41c6
lmessier <leonard.messier@gmail.com>
2016-02-06 00:19:12 +0100

    (CAN-003) Infering driver name from the source setting in Canaillou's contructor

--- src/Canaillou/Canaillou.php
+++ src/Canaillou/Canaillou.php
@@ -3,5 +3,10 @@ namespace Canaillou;
 
 class Canaillou
 {
+    public function __construct($settings = array())
+    {
+        $driverName   = '\Canaillou\Driver\\' . ucfirst($settings['source']);
+        $this->Driver = new $driverName();
+    }
 
 }
