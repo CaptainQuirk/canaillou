656cca179836fc09c136b912bebf61682eccab49
lmessier <leonard.messier@gmail.com>
2016-02-05 23:18:29 +0100

    (CAN-002) Implement base driver class and driver interface

--- src/Canaillou/Driver/Base.php
+++ src/Canaillou/Driver/Base.php
@@ -4,4 +4,16 @@ namespace Canaillou\Driver;
 abstract class Base
 {
     public function __construct() {}
+
+    public function query($item = null, $params = [])
+    {
+        if (empty($item)) {
+            throw new \Exception("\$item parameter is missing or empty");
+        }
+
+        $data   = $this->get();
+        $result = $this->parse($data);
+
+        return $result;
+    }
 }
