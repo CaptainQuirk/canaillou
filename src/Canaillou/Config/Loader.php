ba01e138d36e5967b358e73bb2dfabc159b521af
lmessier <leonard.messier@gmail.com>
2016-02-05 22:46:07 +0100

    (CAN-001) Creating base app skeleton

---
+++ src/Canaillou/Config/Loader.php
@@ -0,0 +1,7 @@
+<?php
+namespace Canaillou\Config;
+
+class Loader
+{
+    public function __construct() {}
+}
---
+++ src/Canaillou/Console/Application.php
@@ -0,0 +1,7 @@
+<?php
+namespace Canaillou\Console;
+
+class Application
+{
+    public function __construct() {}
+}
