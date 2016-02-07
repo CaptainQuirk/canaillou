c7c467f7c54b53f7d6467ff207222c6650b91134
lmessier <leonard.messier@gmail.com>
2016-02-07 17:10:48 +0100

    (CAN-009) Implement a file cache class to store data with a key

---
+++ src/Canaillou/Cache/FileCache.php
@@ -0,0 +1,73 @@
+<?php
+namespace Canaillou\Cache;
+
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
+
+class FileCache
+{
+    private static $instance;
+
+    private static $fs;
+
+    private static $path;
+
+    protected function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (null === static::$instance) {
+            static::$path = getenv('HOME') . DS . '.cache' . DS . 'canaillou';
+            $adapter      = new Local(static::$path);
+            static::$fs   = new Filesystem($adapter);
+
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    public static function store($key, $resource)
+    {
+        $filename = "{$path}_{$data['id']}";
+        static::$fs->write($filename, serialize($data));
+    }
+
+    public static function check($key, $expires)
+    {
+        $instance = static::getInstance();
+
+        $items = scandir(static::$path);
+        $valid = false;
+        foreach ($items as $item) {
+            $fullPath = join(DS, array(static::$path, $item));
+            if (is_dir($fullPath)) {
+                continue;
+            }
+
+            if (preg_match("#^{$key}_[A-Za-z0-9]+$#", $item)) {
+                $content = unserialize(static::$fs->read($item));
+                $now     = new \DateTime("Sun, 07 Feb 2016 16:01:35 GMT");
+                if ($now->getTimestamp() > $content['expires']->getTimestamp()) {
+                    static::$fs->delete($item);
+
+                    break;
+                }
+
+                $valid = true;
+            }
+        }
+
+        return $valid;
+    }
+}
