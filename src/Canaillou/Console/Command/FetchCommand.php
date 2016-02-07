52e1e7fb71503297a523a19fd5f475cb2baf9a62
lmessier <leonard.messier@gmail.com>
2016-02-07 17:11:23 +0100

    (CAN-009) Add a new fetch command to the cli

---
+++ src/Canaillou/Console/Command/FetchCommand.php
@@ -0,0 +1,42 @@
+<?php
+namespace Canaillou\Console\Command;
+
+use ConsoleKit\Command;
+use ConsoleKit\Colors;
+use Canaillou\Canaillou;
+use Canaillou\Cache\FileCache;
+
+/**
+ * Query a source for support
+ *
+ * @opt source  The source to query
+ */
+class FetchCommand extends Command
+{
+    public function execute(array $args, array $options = array())
+    {
+        if (empty($options['source'])) {
+            throw new \Exception("Missing or empty source option");
+        }
+        $source = $options['source'];
+
+        $this->Canaillou = new Canaillou([
+            'source' => $source
+        ]);
+
+        $resource = $this->Canaillou->fetch();
+
+        $expires = new \DateTime($resource->getHeader('Expires')[0]);
+        if (FileCache::check($source, $expires)) {
+            echo Colors::colorize("Data for {$source} is already up to date", Colors::GREEN);
+
+            exit;
+        }
+
+        FileCache::store($source, array(
+            'id'      => str_replace('"', '', $resource->getHeader('Etag')[0]),
+            'expires' => $expires,
+            'content' => (string)$resource->getBody()
+        ));
+    }
+}
