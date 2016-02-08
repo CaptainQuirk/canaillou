1c7e1a6d7d775891255189f73dbea36fd98904f5
lmessier <leonard.messier@gmail.com>
2016-02-08 13:41:53 +0100

    (CAN-010) Creating a registry for composer hook scripts

---
+++ src/Canaillou/Installer/ScriptRegistry.php
@@ -0,0 +1,21 @@
+<?php
+namespace Canaillou\Installer;
+
+use Canaillou\Canaillou;
+use Guzzle\Client;
+
+require_once dirname(dirname(dirname((__FILE__)))) . '/bootstrap.php';
+
+final class ScriptRegistry
+{
+    public static function postInstall()
+    {
+        $supportedDrivers = [ 'caniuse' ];
+
+        foreach ($supportedDrivers as $driver) {
+            $canaillou = new Canaillou([ 'source' => $driver ]);
+            $canaillou = new Canaillou([ 'source' => 'caniuse' ]);
+            $canaillou->fetch();
+        };
+    }
+}
